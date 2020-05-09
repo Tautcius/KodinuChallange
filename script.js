0ece5124f639f2eb5cdd68af34149d4b30b7b608
Tautcius <t.bujauskas@gmail.com>
2020-05-09 16:44:27 +0300

    added matching logic. need to think about asyc

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 import Data from "./data.js"
 
+// reserved for future to select dificulty
+const initialArray = Data.splice(0, Data.length - 2);
+const arrayToShuffle = initialArray.concat(initialArray);
+console.log(arrayToShuffle.length);
+
 // Shuffle data
-let playArray = [];
-console.log(playArray);
-while (Data.length !== 0) {
-    let randomIndex = Math.floor(Math.random() * Data.length);
-    playArray.push(Data[randomIndex]);
-    Data.splice(randomIndex, 1);
+function shuffle(array) {
+    let playArray = [];
+    while (array.length !== 0) {
+        let randomIndex = Math.floor(Math.random() * array.length);
+        playArray.push(array[randomIndex]);
+        array.splice(randomIndex, 1);
+    }
+    return playArray;
+};
+
+const finalArray = shuffle(arrayToShuffle);
+let cardsChosen = [];
+let cardsMatchedId = [];
+let cardsWonId = [];
+// Check for match
+function checkMatch() {
+    let allCards = document.querySelectorAll('.card');
+    console.log(allCards);
+    const firstCard = cardsChosen[0];
+    console.log(firstCard);
+    const secondCard = cardsChosen[1];
+    console.log(secondCard);
+    if (firstCard !== secondCard) {
+        allCards[cardsMatchedId[0]].classList.remove('flip');
+        console.log(allCards[cardsMatchedId[0]]);
+        allCards[cardsMatchedId[1]].classList.remove('flip');
+        console.log(allCards[cardsMatchedId[1]]);
+        console.log("No Match");
+    } else {
+        console.log("It's a Match");
+        cardsWonId = cardsWonId.concat(cardsMatchedId);
+    }
+    cardsChosen = [];
+    cardsMatchedId = [];
 }
-console.log(playArray);
 
 // fliping cards
 function flipCard() {
-    this.classList.toggle('flip')
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(finalArray[cardId].name);
+    console.log(finalArray[cardId].name);
+    cardsMatchedId.push(cardId);
+    console.log(cardId);
+    this.classList.add('flip');
+    if (cardsChosen.length === 2) {
+        checkMatch();
+    }
+
 }
+
 //creating cards
 const grid = document.querySelector('.container')
-
-
-function createGrid() {
-    for (let i = 0; i < 11; i++) {
+console.log(grid);
+function createGrid(arrayToPlay) {
+    for (let i = 0; i < arrayToPlay.length; i++) {
         const card = document.createElement('div');
         const front = document.createElement('div');
         const back = document.createElement('div');
@@ -29,17 +70,16 @@ function createGrid() {
         back.setAttribute('class', 'back');
         card.setAttribute('class', 'card');
         imgKodinu.setAttribute('src', './asset/Kodinu.png');
-        imgGuess.setAttribute('src', './asset/Cars.png');
+        imgGuess.setAttribute('src', arrayToPlay[i].path)
+        card.setAttribute('data-id', i);
         card.addEventListener('click', flipCard);
         grid.appendChild(card).append(front, back);
         back.appendChild(imgKodinu);
         front.appendChild(imgGuess);
     }
 }
-console.log(grid);
-
 
 // Create grid
-createGrid();
+createGrid(finalArray);
 
 
